f5babd129c556386f3631a252630283a21a9b444
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2023-10-31 23:52:35 -0700

    Implementation done plus extra credits.

---
+++ app/src/main/java/edu/uw/ischool/dnh7/quizdroid/AnswerActivity.kt
@@ -0,0 +1,94 @@
+package edu.uw.ischool.dnh7.quizdroid
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.AppCompatActivity
+import edu.uw.ischool.dnh7.quizdroid.data.DataSource
+import edu.uw.ischool.dnh7.quizdroid.databinding.AnswerBinding
+
+data class AnswerState(
+    var currCorrect: Int = 0,
+    var currPosition: Int = 0,
+    var correctValue: Boolean = false,
+    var total: Int = 0,
+    var type: String = "marvel",
+    var correctAnswer: String = "n/a",
+    var userAnswer: String = "n/a"
+)
+class AnswerActivity : AppCompatActivity()  {
+
+    private lateinit var binding: AnswerBinding
+    companion object {
+        const val TYPE = "type"
+        const val POSITION = "position"
+        const val CORRECT_ANSWER = "correct_answer"
+        const val USER_ANSWER = "user_answer"
+        const val LENGTH = "length"
+        const val CORRECT = "correct"
+        var CURR_CORRECT = 0
+        var CURR_POSITION = 0
+        var correct_value = false
+        var TOTAL = 0
+        var type = "marvel"
+        var correctAnswer = "n/a"
+        var userAnswer = "n/a"
+        private var prevState = AnswerState()
+        private var currState = AnswerState()
+        fun revertState() {
+            Log.d("AnswerState", "CURR: " + currState.toString())
+            Log.d("AnswerState", "PREV: " + prevState.toString())
+            currState = prevState.copy()
+        }
+
+        fun resetValue() {
+            currState = AnswerState()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = AnswerBinding.inflate(layoutInflater)
+        prevState = currState.copy()
+        Log.d("AnswerState", "PREV: " + prevState.toString())
+        setContentView(binding.root)
+        currState.correctValue = intent?.extras?.getString(CORRECT).toString().toBooleanStrict()
+        currState.total = intent?.extras?.getString(LENGTH).toString().toInt()
+        currState.type = intent?.extras?.getString(TYPE).toString()
+        currState.correctAnswer = intent?.extras?.getString(CORRECT_ANSWER).toString()
+        currState.userAnswer = intent?.extras?.getString(USER_ANSWER).toString()
+        currState.currPosition = intent?.extras?.getString(POSITION).toString().toInt()
+        if (currState.correctValue) currState.currCorrect+=1
+        Log.d("AnswerState", "CURR: " + currState.toString())
+        binding.correctAnswer.text = currState.correctAnswer
+        binding.userAnswer.text =  currState.userAnswer
+        binding.answerResult.text = getString(R.string.you_have_1_d_out_of_2_d_correct, currState.currCorrect, currState.total)
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, QuestionActivity::class.java)
+            intent.putExtra(QuestionActivity.TYPE, currState.type)
+            intent.putExtra(QuestionActivity.POSITION, (currState.currPosition+1).toString())
+            this.startActivity(intent)
+        }
+        if (currState.currPosition == currState.total - 1) {
+            binding.nextButton.text = "Finish"
+            binding.nextButton.setOnClickListener {
+                resetValue()
+                val intent = Intent(this, MainActivity::class.java)
+                this.startActivity(intent)
+            }
+        }
+        val callback = HandleBackPress(this, true)
+        this.onBackPressedDispatcher.addCallback(this, callback)
+    }
+    private class HandleBackPress(val context: Context, enable: Boolean) : OnBackPressedCallback(enable) {
+        override fun handleOnBackPressed() {
+            val intent = Intent(context, QuestionActivity::class.java)
+            intent.putExtra(QuestionActivity.TYPE, currState.type)
+            intent.putExtra(QuestionActivity.POSITION, (currState.currPosition).toString())
+            if (currState.currPosition == 0) resetValue()
+            revertState()
+            context.startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/edu/uw/ischool/dnh7/quizdroid/QuestionActivity.kt
+++ app/src/main/java/edu/uw/ischool/dnh7/quizdroid/QuestionActivity.kt
@@ -1,10 +1,13 @@
 package edu.uw.ischool.dnh7.quizdroid
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import android.widget.RadioGroup.LayoutParams
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.marginBottom
 import androidx.core.view.setMargins
@@ -12,12 +15,16 @@ import edu.uw.ischool.dnh7.quizdroid.data.DataSource
 import edu.uw.ischool.dnh7.quizdroid.databinding.QuestionBinding
 import kotlin.random.Random
 
-
+data class QuestionState(
+    var type: String = "marvel",
+    var position: Int = 0,
+    var currentAnswer: String = ""
+)
 class QuestionActivity : AppCompatActivity() {
     companion object {
         const val TYPE = "type"
         const val POSITION = "position"
-        var currentAnswer = ""
+        var currState = QuestionState()
     }
     private lateinit var binding: QuestionBinding
 
@@ -26,11 +33,12 @@ class QuestionActivity : AppCompatActivity() {
         binding = QuestionBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val data = DataSource(this).loadQuestions(intent?.extras?.getString(TYPE).toString().lowercase())
-        val position = intent?.extras?.getString(POSITION).toString().toInt()
-        binding.questionTitle.text = data[position].question
+        currState.position = intent?.extras?.getString(POSITION).toString().toInt()
+        currState.type = this.intent?.extras?.getString(TYPE).toString()
+        binding.questionTitle.text = data[currState.position].question
         binding.submitButton.isEnabled = false
-        val answers = data[position].falseAnswer.toMutableList()
-        answers.add(Random.nextInt(0, data[position].falseAnswer.size),data[position].correctAnswer)
+        val answers = data[currState.position].falseAnswer.toMutableList()
+        answers.add(Random.nextInt(0, data[currState.position].falseAnswer.size),data[currState.position].correctAnswer)
 
         val layoutParam = RadioGroup.LayoutParams(
             LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT
@@ -45,13 +53,43 @@ class QuestionActivity : AppCompatActivity() {
             btn.setPadding(32, 32, 32,32)
             btn.setOnCheckedChangeListener { buttonView, isChecked ->
                 if (isChecked) {
-                    currentAnswer = answer
+                    currState.currentAnswer = answer
                     binding.submitButton.isEnabled = true
-                    Log.v("currentAnswer", currentAnswer)
+                    Log.v("currentAnswer", currState.currentAnswer )
                 }
             }
             binding.answerRadioGroup.addView(btn)
         }
-//        Log.v("answers", answers.toString())
+        binding.submitButton.setOnClickListener {
+            val intent = Intent(this, AnswerActivity::class.java)
+            intent.putExtra(AnswerActivity.TYPE, currState.type)
+            intent.putExtra(AnswerActivity.POSITION, currState.position.toString())
+            intent.putExtra(AnswerActivity.CORRECT_ANSWER, data[currState.position].correctAnswer)
+            intent.putExtra(AnswerActivity.USER_ANSWER, currState.currentAnswer)
+            intent.putExtra(AnswerActivity.LENGTH, data.size.toString())
+            intent.putExtra(AnswerActivity.CORRECT, isCorrect(data[currState.position].correctAnswer, currState.currentAnswer).toString())
+            this.startActivity(intent)
+        }
+
+        val callback = HandleBackPress(this, true)
+        this.onBackPressedDispatcher.addCallback(this, callback)
+    }
+
+    private class HandleBackPress(val context: Context, enable: Boolean) : OnBackPressedCallback(enable) {
+        override fun handleOnBackPressed() {
+            if (currState.position == 0) {
+                val intent = Intent(context, MainActivity::class.java)
+                context.startActivity(intent)
+            } else {
+                val intent = Intent(context, QuestionActivity::class.java)
+                AnswerActivity.revertState()
+                intent.putExtra(TYPE, currState.type)
+                intent.putExtra(POSITION, (currState.position-1).toString())
+                context.startActivity(intent)
+            }
+        }
+    }
+    private fun isCorrect(correctAnswer: String, userAnswer: String) : Boolean {
+        return correctAnswer == userAnswer
     }
 }
