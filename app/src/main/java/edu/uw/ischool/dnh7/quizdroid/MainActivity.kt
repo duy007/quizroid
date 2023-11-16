1d7530d95b830ede33c1df4758c4b8387ef44826
Duy Nguyen <duyhoangnguyen2000@gmail.com>
2023-11-16 03:13:55 -0800

    working with JavaIO in kotlin.

--- app/src/main/java/edu/uw/ischool/dnh7/quizdroid/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/dnh7/quizdroid/MainActivity.kt
@@ -2,10 +2,19 @@ package edu.uw.ischool.dnh7.quizdroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.getExternalStoragePublicDirectory
+import android.util.JsonReader
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import edu.uw.ischool.dnh7.quizdroid.adapters.TopicAdapter
 import edu.uw.ischool.dnh7.quizdroid.databinding.ActivityMainBinding
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileReader
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +26,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+//        Environment.getExternalStoragePublicDirectory()
+        val file = File(filesDir, "./questions.json")
+        val inputStream = FileReader(file)
+        inputStream.use {
+            val text = it.readText()
+            val jsonArray = JSONArray(text)
+            val jsonObjectZero = JSONObject(jsonArray.get(0).toString())
+            Log.d("jsonObjectZeroProperties", jsonObjectZero.toString())
+            Log.d("jsonObjectZeroProperties", jsonObjectZero.getString("title"))
+            Log.d("jsonObjectZeroProperties", jsonObjectZero.getString("desc"))
+            Log.d("jsonObjectZeroProperties", jsonObjectZero.getJSONArray("questions").toString())
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getString("text"))
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getString("answer"))
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getJSONArray("answers")[0].toString())
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getJSONArray("answers")[1].toString())
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getJSONArray("answers")[2].toString())
+            Log.d("jsonObjectZeroProperties", JSONObject(jsonObjectZero.getJSONArray("questions").get(0).toString()).getJSONArray("answers")[3].toString())
+        }
         topicRecyclerView = binding.topicRecyclerView
         topicRecyclerView.layoutManager = LinearLayoutManager(this)
         topicRecyclerView.adapter = TopicAdapter(this,(this.application as QuizApp).getTopicRepository().loadTopics(this))
